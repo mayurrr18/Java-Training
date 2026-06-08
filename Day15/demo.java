fa105ab919e545318ca8ee09be10d2cad217eacf
Mayuresh Choudhari <mayureshchoudhari8@gmail.com>
2026-06-08 15:11:37 +0530

    add day12

---
+++ Day15/demo.java
@@ -0,0 +1,18 @@
+
+public class demo {
+    public static void main(String[] args) {
+        
+    
+    int x=1;
+    while (x<6){
+        if(x==3)
+        { 
+            x++;
+            continue;
+            
+        }
+        System.out.println(x);
+        x++;
+    }
+}
+}
